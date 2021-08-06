7e8220480ae22e3ddabef29441a07a99b9ee1257
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-08-06 12:44:15 +0200

    implement date construction function

--- notification-api/helpers.mjs
+++ notification-api/helpers.mjs
@@ -3,3 +3,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT;
+
+export const constructDate = () => {
+  const today = new Date();
+
+  const date = `${today.getFullYear()}-${
+    today.getMonth() + 1
+  }-${today.getDate()}`;
+
+  return date;
+};
