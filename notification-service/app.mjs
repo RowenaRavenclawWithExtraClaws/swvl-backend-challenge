7b065d3f8f91da5746c447d4b2248054cc827812
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-08-06 13:21:06 +0200

    use only one get request and use the handler

--- notification-service/app.mjs
+++ notification-service/app.mjs
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
 import { PORT } from "./helpers.mjs";
+import { getAndPublishNotification } from "./handlers.mjs";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/notification/:id", () => {});
-app.post("/notification/:id/publishers", () => {});
+app.get("/notification", getAndPublishNotification);
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
