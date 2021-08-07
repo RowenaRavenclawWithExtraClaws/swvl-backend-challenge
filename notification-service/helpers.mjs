427465fe99669d2c32b35c8a4fd9c6d33e77440f
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-08-07 18:50:56 +0200

    get MQ url

--- notification-service/helpers.mjs
+++ notification-service/helpers.mjs
@@ -3,13 +3,4 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT;
-
-export const constructDate = () => {
-  const today = new Date();
-
-  const date = `${today.getFullYear()}-${
-    today.getMonth() + 1
-  }-${today.getDate()}`;
-
-  return date;
-};
+export const mqURL = process.env.MQ_URL;
