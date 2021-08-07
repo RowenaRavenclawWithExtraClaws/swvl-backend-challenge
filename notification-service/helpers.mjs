2723b4d33a0ea5f9051cf3adcbaf4fc156ad9793
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-08-07 19:30:25 +0200

    tweak translate function params

--- notification-service/helpers.mjs
+++ notification-service/helpers.mjs
@@ -4,3 +4,8 @@ dotenv.config();
 
 export const PORT = process.env.PORT;
 export const mqURL = process.env.MQ_URL;
+
+export const translate = (lang, titleText, bodyText) => {
+  // handle translation through a translation service or a json file
+  return [titleText, bodyText];
+};
