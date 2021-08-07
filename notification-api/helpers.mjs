fab3e65e822104fc5409489b3c9d721d38bf6d25
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-08-07 18:51:20 +0200

    get

--- notification-api/helpers.mjs
+++ notification-api/helpers.mjs
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT;
+export const mqURL = process.env.MQ_URL;
 
 export const constructDate = () => {
   const today = new Date();
