3e1b002e9ca51e5eaf53986de3195fe08fd4933a
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2021-08-07 18:29:13 +0200

    return the stored notification

--- notification-api/queries.mjs
+++ notification-api/queries.mjs
@@ -4,8 +4,7 @@ import { constructDate } from "./helpers.mjs";
 const { PrismaClient } = Prisma;
 const prisma = new PrismaClient();
 
-export const saveNotification = async (notificationData) => {
+export const saveNotification = async (notificationData) =>
   await prisma.notifications.create({
     data: { ...notificationData, date_created: constructDate() },
   });
-};
